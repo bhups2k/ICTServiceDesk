3f17408c049f210291f89c257131109215272ed9
bhups2k <bhups.bhudia@gmail.com>
2025-10-31 13:18:06 +0000

    Update hdb-notsoreadonly.user.js

--- hdb-notsoreadonly.user.js
+++ hdb-notsoreadonly.user.js
@@ -3,9 +3,11 @@
 // @namespace    https://www.imperial.ac.uk/
 // @version      0.1
 // @description  Allows you to change the MAC address field by disabling the readOnly field.
-// @author       Bhups
+// @author       Bhups Patel
 // @match        https://hdb.ic.ac.uk/Zope/HDB/showmachine_html/*
 // @grant        none
+// @updateURL    https://github.com/bhups2k/ICTServiceDesk/raw/refs/heads/main/hdb-notsoreadonly.user.js
+// @downloadURL  https://github.com/bhups2k/ICTServiceDesk/raw/refs/heads/main/hdb-notsoreadonly.user.js
 // ==/UserScript==
 
 (function() {
