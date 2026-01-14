f14d1e9bd7106e3a6306aab33d0e0b808c65403d
bhups2k <bhups.bhudia@gmail.com>
2026-01-14 17:40:34 +0000

    Update ServiceNow-comments_and_close_notes_replacer.user.js

--- ServiceNow-comments_and_close_notes_replacer.user.js
+++ ServiceNow-comments_and_close_notes_replacer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ServiceNow Comments & Close Notes Auto-Replacer (multi-field, auto-run)
 // @namespace    https://imperial.ac.uk/
-// @version      1.5.9.3
+// @version      1.5.9.4
 // @description  Automatically replace placeholders in Additional Comments and Close Notes textboxes with correct field values for Incident, Case, and RITM without needing to type.
 // @author       Bhups Patel
 // @match        https://servicemgt.imperial.ac.uk/*
