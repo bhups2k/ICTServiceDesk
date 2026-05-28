ce5c08c4108dcd8d76794d8e2b704d731a31a943
Bhups Patel <bhups.patel@imperial.ac.uk>
2026-05-28 09:53:31 +0100

    added clear button

--- ServiceNow-RichTextComments.user.js
+++ ServiceNow-RichTextComments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ServiceNow - Rich Text Toolbar for Comments and Resolution Notes
 // @namespace    https://imperial.ac.uk/
-// @version      6.5.3
+// @version      6.5.4
 // @description  WYSIWYG rich text editor for Additional Comments, Resolution Notes in INC and CS tickets - Rich Text + combined Source & Code tab
 // @author       Bhups Patel
 // @match        https://servicemgt.imperial.ac.uk/*
@@ -180,6 +180,14 @@ const debug = false;
             label: '↻', title: 'Redo',
             style: '',
             action: function () { document.execCommand('redo', false, null); }
+        },
+        {
+            label: 'Clear', title: 'Clear Text',
+            style: 'font-weight:bold;',
+            action: function (editor) {
+                editor.innerHTML = '';
+                editor.focus();
+            }
         },
     ];
 
